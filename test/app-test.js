543a7dff60d512edd71cafd80a10f92333d25cb9
pyan83 <puyanwei@gmail.com>
2017-10-08 18:02:24 +0100

    got a test working with mocha chai and spectrum

--- test/app-test.js
+++ test/app-test.js
@@ -1,33 +1,26 @@
 var Application = require("spectron").Application;
 var assert = require("assert");
 
-var app = new Application({
-  path: "./main.js"
-});
+describe("application launch", function() {
+  this.timeout(10000);
 
-app
-  .start()
-  .then(function() {
-    // Check if the window is visible
-    return app.browserWindow.isVisible();
-  })
-  .then(function(isVisible) {
-    // Verify the window is visible
-    assert.equal(isVisible, true);
-  })
-  .then(function() {
-    // Get the window's title
-    return app.client.getTitle();
-  })
-  .then(function(title) {
-    // Verify the window's title
-    assert.equal(title, "My App");
-  })
-  .then(function() {
-    // Stop the application
-    return app.stop();
-  })
-  .catch(function(error) {
-    // Log any failures
-    console.error("Test failed", error.message);
+  beforeEach(function() {
+    this.app = new Application({
+      path: `${__dirname}/../node_modules/.bin/electron`,
+      args: ["main.js"]
+    });
+    return this.app.start();
   });
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it("shows two initial windows", function() {
+    return this.app.client.getWindowCount().then(function(count) {
+      assert.equal(count, 2);
+    });
+  });
+});
